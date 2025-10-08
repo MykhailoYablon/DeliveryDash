419b7151553e0bb3027c9212beeb425fadd94563
MykhailoYablon <mihayablon@gmail.com>
2025-10-08 11:33:32 +0300

    Added initial move and steer
    Added initial move and steer

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
 
-    public float x, y = 0.01f;
-    public float z = 0;
+    [SerializeField] float steerSpeed = 0.5f;
+    [SerializeField] float moveSpeed = 0.01f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    float steer, move = 0f;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(x, y, z);
-        transform.Rotate(0, 0, 0.05f);
+
+        if (Keyboard.current.wKey.isPressed)
+        {
+            move = 1f;
+
+        }
+        else if (Keyboard.current.sKey.isPressed)
+        {
+            move = -1f;
+        }
+        else if (Keyboard.current.aKey.isPressed)
+        {
+            steer = 0.01f;
+        }
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -0.01f;
+        }
+
+        transform.Rotate(0, 0, steer * steerSpeed);
+        transform.Translate(0, move * moveSpeed, 0);
+        
         
     }
 }
