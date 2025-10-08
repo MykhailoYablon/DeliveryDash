671ae7d4cad60423ce46056bcf5e431d2caf6a8d
MykhailoYablon <mihayablon@gmail.com>
2025-10-08 11:54:11 +0300

    Updated Driver and Added Initial Collision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -3,36 +3,49 @@ using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-
-    [SerializeField] float steerSpeed = 0.5f;
     [SerializeField] float moveSpeed = 0.01f;
-
-    float steer, move = 0f;
+    [SerializeField] float steerSpeed = 0.5f;
 
     void Update()
     {
 
+        Move();
+
+
+
+
+    }
+
+    void Move()
+    {
+        float move = 0f;
+        float steer = 0f;
+
         if (Keyboard.current.wKey.isPressed)
         {
             move = 1f;
-
         }
+
         else if (Keyboard.current.sKey.isPressed)
         {
             move = -1f;
         }
-        else if (Keyboard.current.aKey.isPressed)
+
+        if (Keyboard.current.aKey.isPressed)
         {
-            steer = 0.01f;
-        }
-        else if (Keyboard.current.dKey.isPressed)
-        {
-            steer = -0.01f;
+            steer = 1f;
         }
 
-        transform.Rotate(0, 0, steer * steerSpeed);
-        transform.Translate(0, move * moveSpeed, 0);
-        
-        
+        else if (Keyboard.current.dKey.isPressed)
+        {
+            steer = -1f;
+        }
+
+
+        float moveAmount = move * moveSpeed * Time.deltaTime;
+        float steerAmount = steer * steerSpeed * Time.deltaTime;
+
+        transform.Translate(0, moveAmount, 0);
+        transform.Rotate(0, 0, steerAmount);
     }
 }
